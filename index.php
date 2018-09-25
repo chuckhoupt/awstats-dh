3f2f1dc2b810639ff6c5905a51688aeb6fc4d2c8
Chuck Houpt <chuck@habilis.net>
2018-09-25 19:37:18 -0400

    Default to sort by config-name and separate viewed stats.

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 $DirData = __DIR__ . '/data';
 $AWStatsURL = 'cgi-bin/awstats.pl';
 
+$sort_default = 'config';
+$NotViewed = 'columns';
 $thousands_sep = ',';
 
 include "awstatstotals/awstatstotals.php";
